f0ceac79e5f285ed19520f5fed108dc3b52bd0be
led <yukkicha228@gmail.com>
2026-09-22 01:14:10 +0900

    Update render.js

--- js/render.js
+++ js/render.js
@@ -258,6 +258,8 @@ function drawLEDRectangle(targetCtx, x, y, color) {
 }
 
 function drawImage(displayData, startX, startY, matrix) {
+    console.log("drawImageに渡されたデータ:", displayData);
+    console.log("data:", displayData?.data);
     const data = displayData.data;
     let index = 0;
 
